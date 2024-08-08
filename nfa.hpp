cc62e55e28b854a58dd2cc92852bfc23ee947bb8
Max Goren <65133477+maxgoren@users.noreply.github.com>
2024-08-08 19:22:14 -0400

    rename and tings

--- nfa.hpp
+++ nfa.hpp
@@ -1,19 +1,24 @@
 #ifndef nfa_hpp
 #define nfa_hpp
 #include <iostream>
+#include <iomanip>
 #include <cstring>
 #include <vector>
 #include "deque.hpp"
 using namespace std;
+
+static const int SCAN = -1;
+static const char NSTATE = ' ';
 
 struct NFA {
     int num_states;
     vector<char> ch;
     vector<int> next1;
     vector<int> next2;
+    vector<bool> in_progress;
     NFA(int ns) {
         num_states = ns;
-        ch = vector<char>(ns, ' ');
+        ch = vector<char>(ns, NSTATE);
         next1 = vector<int>(ns, 0);
         next2 = vector<int>(ns, 0);
     }
@@ -21,76 +26,85 @@ struct NFA {
         num_states = 0;
     }
     bool match(string text);
+    void makeInUse(int state) {
+        if (state >= 0 && state < in_progress.size())
+            in_progress[state] = true;
+    }    
+    void makeNotInUse(int state) {
+        if (state >= 0 && state < in_progress.size())
+            in_progress[state] = false;
+    }
+    bool stateInUse(int state) {
+        return (state >= 0 && state < in_progress.size() && in_progress[state]); 
+    }
 };
 
-const int scan = -1;
 bool NFA::match(string txt) {
     int n1, n2;
     deque<char> dq;
-    vector<bool> in_progress(txt.size()+1, false);
+    in_progress = vector<bool>(txt.size()+1, false);
     int j = 0, N = txt.size(), state = next1[0];
-    dq.put(scan);
+    dq.put(SCAN);
     while (state) {
-        cout<<state<<" -> ";
-        if (state == scan) {
+        cout<<((state == -1) ? "scan":to_string(state))<<" -> ";
+        if (state == SCAN) {
             j++;
-            dq.put(scan);
-        } else if (ch[state] == txt[j]) {
-            if (!in_progress[next1[state]]) {
-                cout<<"matched on "<<txt[j]<<endl;
+            dq.put(SCAN);
+        } else if (ch[state] == txt[j] || ch[state] == '.') {
+            cout<<"matched on "<<txt[j]<<endl;
+            if (!stateInUse(next1[state])) {
                 dq.put(next1[state]);
+                makeInUse(next1[state]);
             }
         } else if (ch[state] == ' ') {
             n1 = next1[state];
             n2 = next2[state];
-            if (!in_progress[n1]) {
+            if (!stateInUse(n1)) {
                 dq.push(n1);
+                 makeInUse(n1);
             }
-            if (n1 != n2 && !in_progress[n2])
+            if (n1 != n2 && !stateInUse(n2)) {
                 dq.push(n2);
+                makeInUse(n2);
+            }
         }
         if (dq.empty() || j == N+1) {
             return false;
         }
         state = dq.pop();
-        if (state >= 0 && state < in_progress.size())
-            in_progress[state] = false;
+        makeNotInUse(state);
     }
     return state == 0;
 }
 
-bool contains(NFA nfa, string txt) {
+bool matchAll(NFA nfa, string txt) {
+    bool didmatch = false;
     for (int i = 0; i < txt.length(); i++) {
         if (nfa.match(txt.substr(i))) {
-            return true;
+            cout<<"Match found at index: "<<i<<endl;
+            didmatch = true;
         }
     }
-    return false;
-}
-
-void matchAll(NFA nfa, string txt) {
-    for (int i = 0; i < txt.length(); i++) {
-        if (nfa.match(txt.substr(i)))
-            cout<<"Match found at index: "<<i<<endl;
-    }
+    cout<<endl;
+    return didmatch;
 }
 
 
 void printNFA(NFA nfa) {
     for (int i = 0; i < nfa.num_states; i++) {
-        cout<<i<<" ";
+        cout<<setw(3)<<i<<" ";
     }
     cout<<endl;
     for (int i = 0; i < nfa.num_states; i++) {
-        cout<<nfa.ch[i]<<" ";
+        cout<<setw(3)<<nfa.ch[i]<<" ";
     }
     cout<<endl;
     for (int i = 0; i < nfa.num_states; i++) {
-        cout<<nfa.next1[i]<<" ";
+        cout<<setw(3)<<nfa.next1[i]<<" ";
     }
     cout<<endl;
     for (int i = 0; i < nfa.num_states; i++) {
-        cout<<nfa.next2[i]<<" ";
+        cout<<setw(3)<<nfa.next2[i]<<" ";
     }
     cout<<endl;
 }
