a5790b59f30bb28f25369a181c839efc090266f8
Max Goren <65133477+maxgoren@users.noreply.github.com>
2024-08-08 19:21:44 -0400

    added several more closure operators

--- compiler.hpp
+++ compiler.hpp
@@ -4,8 +4,20 @@
 #include "nfa.hpp"
 using namespace std;
 
-
 class Compiler {
+    private:
+        int d;
+        bool trace;
+        void s(string str) {
+            if (trace) {
+                for (int i = 0; i < d; i++) 
+                    cout<<" ";
+                cout<<str<<endl;
+            }
+        }
+        void e(string str) { ++d; s(str); }
+        void l(string str) { s(str); --d; }
+        void l() { --d; }
     private:
         int j;
         int state;
@@ -20,7 +32,7 @@ class Compiler {
             j++;
         }
         bool letter(char c) {
-            return c != '*' && c != '|' && c != '(' && c != ')' && c != '\0';
+            return c != '?' && c != '.' && c != '+' && c != '*' && c != '|' && c != '(' && c != ')' && c != '\0';
         }
         void setstate(int idx, char c, int n1, int n2) {
             cout<<"Set state: "<<idx<<" for "<<c<<" with "<<n1<<", "<<n2<<endl;
@@ -28,73 +40,101 @@ class Compiler {
             nfa.next1[idx] = n1;
             nfa.next2[idx] = n2;
         }
-        void error() {
-            cout<<"Wagwan Error!"<<endl;
+        void error(string str) {
+            cout<<"Error: "<<str<<endl;
             cout<<"state: "<<state<<endl;
-            cout<<"j:     "<<j<<endl;
+            cout<<"p[j]:  "<<p[j]<<endl;
             exit(0);
         }
+        int closures(int enter_state, int prev_symbol_state) {
+            int result;
+            if (match('*')) {
+                s("adding kleene closure");
+                setstate(state, ' ', state+1, prev_symbol_state);
+                result = state;
+                nfa.next1[enter_state-1] = state++;
+                nextChar(); 
+            } else if (match('+')) {
+                s("adding 1 or more closure");
+                setstate(state, ' ', state+1, prev_symbol_state);
+                result = state++;
+                nextChar();
+            } else if (match('?')) {
+                s("0 or 1 closure");
+                setstate(state, ' ', state+1, state+1);
+                result = state;
+                nfa.next1[enter_state-1] = state++;
+                nextChar();
+            } else {
+                 result = prev_symbol_state;
+            }
+            l("returning " + to_string(result) + "from factor");
+            return result;
+        }
         int factor() {
-            int t1, t2, r;
-            t1 = state;
+            e("factor()");
+            int enter_state, prev_symbol_state;
+            enter_state = state;
             if (match('(')) {   // (expr)
                 nextChar();
-                t2 = expression();
-                if (match(')')) 
-                    nextChar(); 
-                else {
-                    cout<<"Missing close paren"<<endl;
-                    error();
-                }
+                prev_symbol_state = expression();
+                if (match(')')) nextChar(); 
+                else error("Missing close paren");
             } else if (letter(p[j])) { // e, r -> er
+                string m = "adding state for ";
+                m.push_back(p[j]);
+                s(m);
                 setstate(state,p[j], state+1,state+1);
-                t2 = state; 
+                prev_symbol_state = state; 
                 nextChar(); 
                 state++;
-            } else {
-                cout<<"Invalid character: '"<<p[j]<<"'"<<endl;
-                error();
-            }
-            if (match('*')) {
-                setstate(state, ' ', state+1, t2);
-                r = state;
-                nfa.next1[t1-1] = state;
-                nextChar(); 
+            } else if (match('.')) {
+                s("adding state for wildcard");
+                setstate(state, p[j], state+1, state+1);
+                prev_symbol_state = state;
+                nextChar();
                 state++;
             } else {
-                 r = t2;
+                error("Invalid symbol detected");
+                return -9;
             }
-            return r;
+            return closures(enter_state, prev_symbol_state);
         }
         int term() {
             int t, r;
+            e("term()");
             r = factor();
-            if (match('(') || letter(p[j])) t = term();
+            if (match('(') || letter(p[j]) || match('.')) t = term();
+            l("returning " + to_string(r) + " from term");
             return r;
         }
         int expression() {
+            e("expression()");
             int t1, t2, r;
             t1 = term();
             r = t1;
             if (match('|')) {
+                s("adding or state");
                 nextChar();
                 state++;
                 t2 = state; r = t2; state++;
                 setstate(t2, ' ', expression(), t1);
                 setstate(t2-1, ' ', state, state);
             }
+            l("returning " + to_string(r) + " from expression ");
             return r;
         }
         void init(string pat) {
+            d = 0;
             p = pat;
             j = 0;
             state = 0;
             nfa = NFA(p.size()+1);
-            cout<<"Ok, initialized for "<<p<<endl;
+            cout<<"Initializing NFA for "<<p<<endl;
         }
     public:
-        Compiler() {
-
+        Compiler(bool tracemsg = false) {
+            trace = tracemsg;
         }
         NFA compile(string pat) {
             init(pat);
@@ -102,6 +142,7 @@ class Compiler {
             int t = expression();
             setstate(0, ' ', t, t);
             setstate(state, ' ', 0, 0);
+            printNFA(nfa);
             return nfa;
         }
 
